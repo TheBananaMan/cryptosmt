0e5f4c87b965f2ee03e4004e49a67c14a43d93b9
Ralph Ankele <ralph.ankele.2015@live.rhul.ac.uk>
2017-04-21 11:33:00 +0100

    update sparxround with more weight variables

--- ciphers/sparxround.py
+++ ciphers/sparxround.py
@@ -23,7 +23,7 @@ class SPARXRoundCipher(AbstractCipher):
         """
         Returns the print format.
         """
-        return ['X0', 'X1', 'Y0', 'Y1', 'X0A', 'X1A', 'Y0A', 'Y1A', 'X0L', 'X1L','w']
+        return ['X0', 'X1', 'Y0', 'Y1','X0L', 'X1L','w']
 
     def createSTP(self, stp_filename, parameters):
         """
@@ -44,39 +44,38 @@ class SPARXRoundCipher(AbstractCipher):
             # x0, x1 = left, y0, y1 = right 
             x0 = ["X0{}".format(i) for i in range(rounds + 1)]
             x1 = ["X1{}".format(i) for i in range(rounds + 1)]
-            x0_after_A = ["X0A{}".format(i) for i in range(rounds + 1)]
-            x1_after_A = ["X1A{}".format(i) for i in range(rounds + 1)]
             x0_after_L = ["X0L{}".format(i) for i in range(rounds + 1)]
             x1_after_L = ["X1L{}".format(i) for i in range(rounds + 1)]
             y0 = ["Y0{}".format(i) for i in range(rounds + 1)]
             y1 = ["Y1{}".format(i) for i in range(rounds + 1)]
-            y0_after_A = ["Y0A{}".format(i) for i in range(rounds + 1)]
-            y1_after_A = ["Y1A{}".format(i) for i in range(rounds + 1)]
 
             # w = weight
-            w = ["w{}".format(i) for i in range(rounds)]
+            w = ["w{}".format(i) for i in range(2*rounds)]
 
             stpcommands.setupVariables(stp_file, x0, wordsize)
             stpcommands.setupVariables(stp_file, x1, wordsize)
-            stpcommands.setupVariables(stp_file, x0_after_A, wordsize)
-            stpcommands.setupVariables(stp_file, x1_after_A, wordsize)
             stpcommands.setupVariables(stp_file, x0_after_L, wordsize)
             stpcommands.setupVariables(stp_file, x1_after_L, wordsize)
             stpcommands.setupVariables(stp_file, y0, wordsize)
             stpcommands.setupVariables(stp_file, y1, wordsize)
-            stpcommands.setupVariables(stp_file, y0_after_A, wordsize)
-            stpcommands.setupVariables(stp_file, y1_after_A, wordsize)
             stpcommands.setupVariables(stp_file, w, wordsize)
 
             stpcommands.setupWeightComputation(stp_file, weight, w, wordsize)
 
             for i in range(rounds):
-                self.setupSPARXRound(stp_file, x0[i], x1[i], y0[i], y1[i],
-                                     x0_after_A[i], x1_after_A[i],
+                #do round function left (SPECKEY)
+                self.setupSPECKEYRound(stp_file, x0[i], x1[i], x0[i+1], x1[i+1],
+                                       w[i], wordsize)
+
+                #do round function right (SPECKEY)
+                self.setupSPECKEYRound(stp_file, y0[i], y1[i], y0[i+1], y1[i+1],
+                                       w[rounds+i], wordsize)
+
+                if (rounds % self.rounds_per_step) == 0:
+                    #every step do L-box and feistel
+                    self.setupSPARXRound(stp_file, x0[i], x1[i], y0[i], y1[i],
                                      x0_after_L[i], x1_after_L[i],
-                                     y0_after_A[i], y1_after_A[i],
-                                     x0[i+1], x1[i+1], y0[i+1], y1[i+1],
-                                     w[i], wordsize, rounds)
+                                     x0[i+1], x1[i+1], y0[i+1], y1[i+1])
 
             # No all zero characteristic
             stpcommands.assertNonZero(stp_file, x0+x1+y0+y1, wordsize)
@@ -100,48 +99,34 @@ class SPARXRoundCipher(AbstractCipher):
         return
 
     def setupSPARXRound(self, stp_file, x0_in, x1_in, y0_in, y1_in,
-                        x0_after_A, x1_after_A, x0_after_L, x1_after_L,
-                        y0_after_A, y1_after_A, x0_out, x1_out, y0_out, y1_out,
-                        w, wordsize, rounds):
+                        x0_after_L, x1_after_L,
+                        x0_out, x1_out, y0_out, y1_out):
         """
         Model for differential behaviour of one step SPARX
         """
         command = ""
+        command += self.L(x0_in, x1_in, x0_after_L, x1_after_L)
 
-        # left
-        command += self.A(x0_in, x1_in, x0_after_A, x1_after_A, w, wordsize)
-        #right
-        command += self.A(y0_in, y1_in, y0_after_A, y1_after_A, w, wordsize)
+        #Assert(x_out = L(A^a(x_in)) xor A^a(y_in))
+        command += "ASSERT(" + x0_out + " = "
+        command += "BVXOR(" + x0_after_L + " , " + y0_in + ")"
+        command += ");\n"
+        command += "ASSERT(" + x1_out + " = "
+        command += "BVXOR(" + x1_after_L + " , " + y1_in + ")"
+        command += ");\n"
 
-        # every step
-        if (rounds % self.rounds_per_step) == 0:
-            command += self.L(x0_after_A, x1_after_A, x0_after_L, x1_after_L)
-
-            #Assert(x_out = L(A^a(x_in)) xor A^a(y_in))
-            command += "ASSERT(" + x0_out + " = "
-            command += "BVXOR(" + x0_after_L + " , " + y0_after_A + ")"
-            command += ");\n"
-            command += "ASSERT(" + x1_out + " = "
-            command += "BVXOR(" + x1_after_L + " , " + y1_after_A + ")"
-            command += ");\n"
-
-            #Assert(y_in = A^a(x_in)
-            command += "ASSERT({} = {});\n".format(y0_out, x0_after_A)
-            command += "ASSERT({} = {});\n".format(y1_out, x1_after_A)
-        else:
-            command += "ASSERT({} = {});\n".format(x0_after_A, x0_out)
-            command += "ASSERT({} = {});\n".format(x1_after_A, x1_out)
-            command += "ASSERT({} = {});\n".format(y0_after_A, y0_out)
-            command += "ASSERT({} = {});\n".format(y1_after_A, y1_out)
+        #Assert(y_out = A^a(x_in))
+        command += "ASSERT({} = {});\n".format(y0_out, x0_in)
+        command += "ASSERT({} = {});\n".format(y1_out, x1_in)
 
         stp_file.write(command)
         return
 
 
-    def A(self, x_in, y_in, x_out, y_out, w, wordsize):
+    def setupSPECKEYRound(self, stp_file, x_in, y_in, x_out, y_out, w, wordsize):
         """
-        Model for the ARX box (round) function of SPARX. A^a denotes a 
-        rounds of SPECKEY.
+        Model for the ARX box (round) function of SPARX which is the 
+        same as SPECKEY.
         """
         command = ""
 
@@ -163,7 +148,8 @@ class SPARXRoundCipher(AbstractCipher):
                                            y_in, x_out)
         command += ");\n"
 
-        return command
+        stp_file.write(command)
+        return
 
     def L(self, x_in, y_in, x_out, y_out):
         """
